88df6ba4161b9f0534532da279f7c97ecdcdfc00
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-01-06 17:27:33 -0800

    Done with org classify ...

---
+++ R/org_classify_description_texts.R
@@ -0,0 +1,433 @@
+#' Import machine-readable data from electronic 990 forms filed with the IRS
+#'
+#' @param year A year in which a form was filed  
+#' 
+#' @return If successful, the function returns a XML file it contains the 990 forms filed in a particular year.
+#' @importFrom glue glue 
+#' @importFrom jsonlite fromJSON
+#' @importFrom dplyr mutate
+#' @export
+
+import_idx <- function(year){
+
+  # avoid non-numeric year argument
+  if (!is.numeric(year)) {
+    stop("year argument should be numeric")
+  }
+
+  else {
+    # url vector
+    ## the value range for the year variable is between 2011 and 2020
+    url_vars <- glue("https://s3.amazonaws.com/irs-form-990/index_{year}.json")
+
+    # select url and import data
+    idx <- fromJSON(url_vars)[[1]] %>%
+      mutate(IRS_year = year)
+
+    message(glue("successfully importing year {year} data. You should assign this outcome to idx."))
+
+    return(idx)
+  }
+}
+
+idx <- import_idx(2012)
+
+#' Get Employer Identification Numbers (EINs) associated with foundations 
+#'
+#' @param idx A XML file it contains the 990 forms filed in a particular year. An outcome of import_idx() function.
+#' 
+#' @return If successful, the function returns the EINs associated with foundations. 
+#' @importFrom dplyr filter
+#' @importFrom dplyr pull
+#' @export
+
+get_foundation_ein <- function(idx){
+
+  foundation_ein <- idx %>%
+    filter(FormType == "990PF") %>%
+    pull(EIN)
+
+  return(foundation_ein)
+
+}
+
+#' Get the Amazon Web Server URL associated with a particular Employment Identification Numbers 
+#'
+#' @param ein An Employment Identification Numbers 
+#' @param form The default is NULL. There are three other options: "990", "990PF", "990EZ"
+#' 
+#' @return If successful, the function returns the Amazon Web Server URL associated with a particular Employment Identification Numbers.
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#' @importFrom glue glue
+#' @export
+
+get_aws_url <- function(ein, form = NULL) {
+
+  # Turn search parameter into character vector
+  ein <- ifelse(!is.character(ein), as.character(ein), ein)
+
+  # Some organizations have two object IDs
+
+  if (exists("idx") == FALSE) {
+    stop("You first need to import IRS data and assign it to an object named idx using the import_idx() function.")
+  }
+
+  if (is.null(form)) {
+  obj_id <- idx %>%
+    filter(EIN == ein) %>%
+    select(ObjectId)
+  } else {
+    obj_id <- idx %>%
+      filter(FormType == form) %>%
+      filter(EIN == ein) %>%
+      select(ObjectId)
+  }
+
+  # Glue search parameter and the rest of the URL together
+
+  # This tax report was amended
+  if (nrow(obj_id == 2)) {
+
+    # glue("http://s3.amazonaws.com/irs-form-990/{obj_id[1,]}_public.xml") # Pre-amended report
+
+    glue("http://s3.amazonaws.com/irs-form-990/{obj_id[2,]}_public.xml") # Post-amended IRS report
+  }
+
+  # This tax report was not amended
+  if (nrow(obj_id == 1)) {
+    glue("http://s3.amazonaws.com/irs-form-990/{obj_id[1,]}_public.xml")
+  }
+
+}
+
+#' Get the XML root element associated with a particular Employment Identification Numbers 
+#'
+#' @param ein An Employment Identification Numbers 
+#' 
+#' @return If successful, the function returns the XML root element associated with a particular Employment Identification Numbers 
+#' @importFrom XML xmlTreeParse
+#' @importFrom XML xmlRoot
+#' @export
+
+get_990 <- function(ein) {
+
+  ## lookup AWS object
+  xml_root <- get_aws_url(ein) %>%
+    ## get page and parse xml
+    xmlTreeParse() %>%
+    ## get root
+    xmlRoot()
+
+  return(xml_root)
+}
+
+#' Get the name of the organization associated with a particular Employment Identification Numbers 
+#'
+#' @param ein An Employment Identification Numbers 
+#' 
+#' @return If successful, the function returns the name of the organization associated with a particular Employment Identification Numbers
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#' @export
+
+get_organization_name_990 <- function(ein) {
+
+  # IRS data from a particular year
+  idx <- import_idx(year)
+
+  organization_name <- idx %>%
+    filter(EIN == ein) %>%
+    select(OrganizationName)
+
+  return(organization_name)
+}
+
+#' Standardize the website URL of an organization 
+#'
+#' @param raw_website The raw website URL of an organization 
+#' 
+#' @return If successful, the function returns the standardized version of the raw website URL. 
+#' @importFrom rex rex 
+#' @importFrom rex except_some_of
+#' @importFrom rex group
+#' @importFrom rex maybe
+#' @importFrom rex zero_or_more
+#' @importFrom rex one_or_more
+#' @importFrom rex at_least
+#' @importFrom rex between
+#' @importFrom urltools url_parse
+#' @importFrom glue glue_data
+#' @importFrom dplyr mutate
+#' @importFrom dplyr filter 
+#' @importFrom stringr str_replace_all
+#' @export
+
+standardize_url <- function(raw_website){
+  
+  # Lower case and remove whitespace
+  raw_website <- raw_website %>% tolower() %>% trimws()
+  
+  # Standardize NAs
+  na_tested <- replace(raw_website, raw_website %in% c("na", "n/a", "none"), NA)
+  
+  if (is.na(na_tested) == TRUE) return(NA)
+  
+  if (is.na(na_tested) == FALSE) {
+    
+    if (grepl(re, na_tested) == TRUE) {
+      return(na_tested)
+    }
+    
+    else {
+      
+      # Standardize more fuzzy cases 
+      url_fixed <- na_tested %>%
+        url_parse()
+      
+      if (sum(is.na(url_fixed)) == 5) {
+        
+        out <- url_fixed %>%
+          mutate(scheme = "http://") %>%
+          glue_data("{scheme}{domain}")
+        
+        return(out)
+        
+      }
+      
+      else {
+        out <- url_fixed %>%
+          mutate(path = str_replace_all(path, "http|/| ", "")) %>%
+          mutate(path = replace(path, path %in% c("gmail.com", "hotmail.com"), NA)) %>%
+          filter(is.na(path) == FALSE) %>%
+          mutate(scheme = "http://") %>%
+          glue_data("{scheme}{path}") 
+        
+        return(out)
+      }
+      
+    }
+    
+  }
+}
+
+#' Clean the descriptions of the programs run by a particular organization
+#'
+#' @param program_desc Program descriptions 
+#' @param text_length_threshold The length of the minimum words to filter the program descriptions 
+#' 
+#' @return If successful, the function returns the cleaned descriptions of the programs run by a particular organization 
+#' @importFrom purrr map_int 
+#' @export
+
+clean_program_desc <- function(program_desc, text_length_threshold) {
+  
+  if (length(program_desc) > 0) {
+    length_check <- map_int(
+      program_desc,
+      function(x) {
+        nchar(x) > text_length_threshold
+      }
+    )
+
+    program_desc <- program_desc[length_check]
+  } else {
+    program_desc <- NA
+  }
+
+  return(program_desc[!duplicated(program_desc)])
+}
+
+#' Simplify ifelse process 
+#'
+#' @param var target variable (likely character type) 
+#' 
+#' @return if the var length is 0 then the function returns NA, otherwise it returns the input parameter.
+#' @export
+
+ifnotNA <- function(var) {
+  ifelse(length(var) != 0,
+         var, NA
+  )
+}
+
+#' Get concrete information from 990 forms 
+#'
+#' @param xml_root An XML root element associated with a particular organization
+#' @param type A type of concrete information. It should be either "website" (website URL), "mission_desc," (mission statement) or "program_desc" (program description)
+#' @param text_length_threshold The length of the minimum words associated with a particular organization. This value is used to filter the program descriptions. The default value is 50. 
+#' 
+#' @return Depending on the type parameter, the function returns either a website URL, a mission statement, or a program description(s).
+#' @importFrom purrr pluck
+#' @importFrom purrr map_chr
+#' @importFrom XML getNodeSet
+#' @export
+
+get_value_990 <- function(xml_root, type =
+                            c("website",
+                              "mission_desc",
+                              "program_desc"),
+                          text_length_threshold = 50) {
+
+  xml_plucked <- xml_root %>%
+    pluck(2) # pick the second element on the list
+
+  # Outcomes
+  if (type == "website") {
+    website <- xml_plucked %>% getNodeSet("//WebsiteAddressTxt") %>% map_chr(xmlValue)
+    return(ifnotNA(website) %>% standardize_url)
+  }
+
+  if (type == "mission_desc") {
+    mission_desc <- xml_plucked %>% getNodeSet("//MissionDesc") %>% map_chr(xmlValue)
+    return(ifnotNA(mission_desc))
+  }
+
+  if (type == "program_desc") {
+    program_desc <- xml_plucked %>% getNodeSet("//Desc") %>% map_chr(xmlValue) %>%
+      clean_program_desc(text_length_threshold)
+    return(ifnotNA(program_desc))
+  }
+}
+
+#' Get concrete information from Schedule R
+#'
+#' @param ein An Employment Identification Numbers 
+#' @return The function returns either concrete information from Schedule R (likely character) or states that such information is not present.
+#' @importFrom purrr pluck
+#' @importFrom purrr map_chr
+#' @importFrom stringr str_detect
+#' @importFrom XML getNodeSet
+#' @importFrom XML xmlChildren
+#' @importFrom glue glue 
+#' @export
+
+get_scheduleR <- function(ein) {
+
+  parsed_xml <- get_990(ein) %>%
+    pluck(2)
+
+  # ScheduleR is present
+  if (sum(str_detect(
+    names(parsed_xml),
+    "ScheduleR"
+  )) == 1) {
+
+    # Go down to the children level
+    parsed_scheduleR <- parsed_xml %>%
+      xmlChildren()
+
+    # Select ScheduleR and go to the node EIN
+    out <- parsed_scheduleR$IRS990ScheduleR %>%
+      getNodeSet("//EIN") %>%
+      map_chr(xmlValue) %>%
+      list() # The output type is a list because it can be a vector of many numeric values
+
+    # ScheduleR is not present
+  } else {
+    out <- list(glue("{ein} did not file ScheduleR."))
+  }
+
+  return(ifnotNA(out))
+}
+
+#' Get concrete information from Schedule O
+#'
+#' @param ein An Employment Identification Numbers 
+#' @return The function returns either concrete information from Schedule O (likely character) or states that such information is not present.
+#' @importFrom purrr pluck
+#' @importFrom purrr map_chr
+#' @importFrom stringr str_detect
+#' @importFrom XML getNodeSet
+#' @importFrom XML xmlChildren
+#' @importFrom glue glue 
+#' @export
+
+get_scheduleO <- function(ein) {
+
+  parsed_xml <- get_990(ein) %>%
+    pluck(2)
+
+  # ScheduleO is present
+  if (sum(str_detect(
+    names(parsed_xml),
+    "ScheduleO"
+  )) == 1) {
+
+    # Go down to the children level
+    parsed_scheduleO <- parsed_xml %>%
+      xmlChildren()
+
+    # Select ScheduleO and go to the node EIN
+    out <- parsed_scheduleO$IRS990ScheduleO %>%
+      getNodeSet("//ExplanationTxt") %>%
+      map_chr(xmlValue) %>%
+      list() # The output type is a list because it can be a vector of many numeric values
+
+    # ScheduleO is not present
+  } else {
+    out <- list(glue("{ein} did not file ScheduleO."))
+  }
+
+  return(ifnotNA(out))
+}
+
+#' Get various information about an organization 
+#'
+#' @param ein An Employment Identification Numbers (EIN)
+#' @param type Different types of information users can get about an organization: "base," "extended," "unnested," and "combined". A base type output includes an organization's EIN, standardized website URL, its mission statement and program descriptions. An extended version includes information on an organization's schedule R and O documents (list columns). An unnested version transforms the information on schedule R and O documents as character vectors, not as list columns. A combined version joins the base and extended outputs together. 
+#' @return The function returns either concrete information from Schedule O (likely character) or states that such information is not present.
+#' @importFrom tibble tibble 
+#' @importFrom tidyr unnest
+#' @importFrom dplyr left_join
+#' @export
+
+example_function_for_single_org <- function(ein, type = c("base",
+                                                          "extended",
+                                                          "unnested",
+                                                          "combined"))
+{
+
+  xml_root <- get_990(ein)
+
+  # website
+  website <- get_value_990(xml_root, "website")
+
+  # mission statement
+  mission_desc <- get_value_990(xml_root, "mission_desc")
+
+  # program description
+  program_desc <- get_value_990(xml_root, "program_desc")
+
+  # ScheduleR
+  schedule_r <- get_scheduleR(ein)
+
+  # ScheduleO
+  schedule_o <- get_scheduleO(ein)
+
+  # put results together as a data.frame
+
+  ## Unnested columns
+  out <- tibble(
+    "EIN" = ein,
+    "website_address" = website,
+    "mission_descriptions" = mission_desc,
+    "program_descriptions" = program_desc
+  )
+
+  ## Nested columns
+  nest_out <- tibble(
+    "EIN" = ein,
+    "ScheduleR" = schedule_r,
+    "ScheduleO" = schedule_o
+  )
+
+  # Outputs
+  if (type == "base") return(out)
+  if (type == "extended") return(nest_out)
+  if (type == "unnested") return(nest_out %>%
+                                   unnest(ScheduleR) %>%
+                                   unnest(ScheduleO))
+  if (type == "combined") return(out %>% left_join(nest_out))
+
+}
