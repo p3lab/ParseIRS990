3679eb0247f540cd308c87967ef3c75ec0bf85f0
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-01-19 11:20:51 -0800

    Add unit testing for the URL standardization problem

--- tests/testthat/test-get-value-990.R
+++ tests/testthat/test-get-value-990.R
@@ -1,12 +1,13 @@
 library(testthat)
 library(ParseIRS990)
 
-#context("")
+context("Standaridze an organization's website address (URL)")
 
-#test_that("", {
+test_that("MoveOn example check", {
     
- #   idx <- import_idx(2019)
-  #  xml_root <- get_990("061553389")
-   # get_value_990(xml_root)
-    
-#})
+  idx <- import_idx(2019)
+  xml_root <- get_990("061553389")
+  expect_condition 
+  expect_equal(grepl("http", get_value_990(xml_root, type = "website")) == TRUE, TRUE)    
+  
+})
