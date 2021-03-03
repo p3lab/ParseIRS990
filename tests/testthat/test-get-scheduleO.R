cad11f5f6d87cb33e904578f9cb8475ef4aa45b7
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-03-02 19:28:36 -0800

    replaced external with internal data

--- tests/testthat/test-get-scheduleO.R
+++ tests/testthat/test-get-scheduleO.R
@@ -1,19 +1,11 @@
+#library(usethis)
 library(testthat)
 library(ParseIRS990)
 
-context("Standaridze an organization's website address (URL)")
+#idx <- import_idx(2019)
+#usethis::use_data(idx, internal = TRUE) # Stores all data in a single object 
 
-test_that("MoveOn example check", {
-  
-  expect_equal(grepl("http", standardize_url("www.moveon.org")), TRUE)
-  
-    })
-
-grepl("http", standardize_url("www.moveon.org"))
 test_that("Check whether the exact document source is provided when inspecting Schedule O", {
-    
-    # 2019 IRS data 
-    data("idx_2019")
     
     expect_equal(unique(grepl("From FORM", get_scheduleO("061553389"))), TRUE)
 
@@ -21,6 +13,6 @@ test_that("Check whether the exact document source is provided when inspecting S
 
 test_that("Check whether get_scheduleO returns all entries", {
   
-expect_equal(length(grepl("From FORM", (get_scheduleO("061553389")), TRUE)) >= 2, TRUE)
+  expect_equal(length(grepl("From FORM", (get_scheduleO("061553389")), TRUE)) >= 2, TRUE)
   
   })
